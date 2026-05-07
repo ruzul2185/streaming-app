78a69d26f830223f5693704939f32ceeef4d4fe2
ruzul2185 <ruzulchaure2185@gmail.com>
2026-05-08 00:58:39 +0530

    started with user functionality

--- server/controllers/movie_controller.go
+++ server/controllers/movie_controller.go
@@ -6,6 +6,7 @@ import (
 	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
 	"github.com/ruzul2185/streaming-app/server/database"
 	"github.com/ruzul2185/streaming-app/server/models"
 	"go.mongodb.org/mongo-driver/v2/bson"
@@ -13,6 +14,8 @@ import (
 )
 
 var movieCollection *mongo.Collection = database.OpenCollection("movies")
+
+var validate = validator.New()
 
 func GetMovies() gin.HandlerFunc {
 	return func(c *gin.Context) {
@@ -59,5 +62,36 @@ func GetMovie() gin.HandlerFunc {
 		}
 
 		c.JSON(http.StatusOK, movie)
+	}
+}
+
+func AddMovie() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
+
+		var movie models.Movie
+
+		if err := c.ShouldBindJSON(&movie); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid input"})
+			return
+		}
+
+		if err := validate.Struct(movie); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error":   "Validation failed",
+				"details": err.Error(),
+			})
+			return
+		}
+
+		result, err := movieCollection.InsertOne(ctx, movie)
+
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to add movie"})
+			return
+		}
+
+		c.JSON(http.StatusCreated, result)
 	}
 }
---
+++ server/controllers/user_controller.go
@@ -0,0 +1,80 @@
+package controllers
+
+import (
+	"context"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
+	"github.com/ruzul2185/streaming-app/server/database"
+	"github.com/ruzul2185/streaming-app/server/models"
+	"go.mongodb.org/mongo-driver/v2/bson"
+	"go.mongodb.org/mongo-driver/v2/mongo"
+	"golang.org/x/crypto/bcrypt"
+)
+
+var userCollection *mongo.Collection = database.OpenCollection("users")
+
+func HashPassword(password string) (string, error) {
+	HashPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return "", err
+	}
+
+	return string(HashPassword), nil
+}
+
+func RegisterUser() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var user models.User
+
+		if err := c.ShouldBindJSON(&user); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid input data"})
+			return
+		}
+		validate := validator.New()
+
+		if err := validate.Struct(user); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Validation failed", "details": err.Error()})
+			return
+		}
+
+		hashedPassword, err := HashPassword(user.Password)
+
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Unable to hash password"})
+			return
+		}
+
+		ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
+
+		count, err := userCollection.CountDocuments(ctx, bson.M{"email": user.Email})
+
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to check existing user"})
+			return
+		}
+
+		if count > 0 {
+			c.JSON(http.StatusConflict, gin.H{"error": "User already exists"})
+			return
+		}
+
+		user.UserID = bson.NewObjectID().Hex()
+		user.Password = hashedPassword
+		user.CreatedAt = time.Now()
+		user.UpdatedAt = time.Now()
+
+		result, err := userCollection.InsertOne(ctx, user)
+
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"error": "Failed to create user",
+			})
+		}
+
+		c.JSON(http.StatusCreated, result)
+	}
+}
